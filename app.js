7ffa045d4d9f8ae34884927c6810b0d44de18c44
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-04-09 11:43:49 +0530

    new files

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js")
+
+const MONGO_URL = 'mongodb+srv://pulkitd132:qA7ZZBlBnH2fO8eD@cluster0.vcf0mow.mongodb.net/wanderlust';
+
+main()
+    .then(() =>{
+        console.log("Connected to database");
+    })
+    .catch((err) =>{
+        console.log(err);
+    });
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
 
 
 app.get('/', (req,res)=>{
     res.send("Hey our app is working fine");
+});
+
+app.get('/testListing', async (req,res) =>{
+    let sampleListing = new Listing({
+        title: "My New Villa",
+        description: "By the beach",
+        price: 1500000,
+        location: "Calangute Goa",
+        country: "India",
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("Successfully testing");
 });
 
 app.listen(8080,  ()=>{
