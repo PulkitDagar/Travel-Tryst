bcda79aa6b89541ac3eaffddd0a6eb275b9b738a
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-04-09 12:36:21 +0530

    new files

--- app.js
+++ app.js
@@ -22,18 +22,22 @@ app.get('/', (req,res)=>{
     res.send("Hey our app is working fine");
 });
 
-app.get('/testListing', async (req,res) =>{
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the beach",
-        price: 1500000,
-        location: "Calangute Goa",
-        country: "India",
-    });
+// app.get('/testListing', async (req,res) =>{
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the beach",
+//         price: 1500000,
+//         location: "Calangute Goa",
+//         country: "India",
+//     });
 
-    await sampleListing.save();
-    console.log("sample was saved");
-    res.send("Successfully testing");
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("Successfully testing");
+// });
+
+app.get("/listing", async (req,res) => {
+    const allListing = await Listing.find({});
 });
 
 app.listen(8080,  ()=>{
---
+++ init/index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = 'mongodb+srv://pulkitd132:qA7ZZBlBnH2fO8eD@cluster0.vcf0mow.mongodb.net/wanderlust';
+
+main()
+    .then(() =>{
+        console.log("Connected to database");
+    })
+    .catch((err) =>{
+        console.log(err);
+    });
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+// const initDB = async() => {
+//     await Listing.deleteMany({});
+//     await Listing.insertMany(initData.data);
+//     console.log("Data is Initialized");
+// };
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    const transformedData = initData.data.map(listing => ({
+        ...listing,
+        image: listing.image.url // Assuming the 'url' property contains the URL string
+    }));
+    await Listing.insertMany(transformedData);
+    console.log("Data is Initialized");
+};
+
+initDB();
