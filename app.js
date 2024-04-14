9b5bc091b1cf42fe2925f2837ec027ce0f615a86
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-04-14 10:02:30 +0530

    changes

--- app.js
+++ app.js
@@ -74,9 +74,8 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 
 //Create Route
 app.post("/listings", wrapAsync(async (req, res, next) => {
-        if(!req.body.listing){
-            throw new ExpressError(400,"Send valid data for listing");
-        }
+        let result = listingSchema.validate(req.body);
+        console.log(result);
         const newListing = new Listing(req.body.listing);
         await newListing.save();
         res.redirect("/listings");
