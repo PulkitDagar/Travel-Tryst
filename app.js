1888e0a587a94c4e606777aaa2d6f1c9eb1254cd
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-04-15 13:56:59 +0530

    changes

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js")
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema, reviewSchema} = require("./schema.js");
-const Review = require("./models/review.js");
 
-const listings = require("./routes/listing.js")
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 
 const MONGO_URL = 'mongodb+srv://pulkitd132:qA7ZZBlBnH2fO8eD@cluster0.vcf0mow.mongodb.net/wanderlust';
@@ -41,56 +39,14 @@ app.get('/', (req,res)=>{
 
 
 
-const validateReview = (req,res,next) => {
-    let {error} = reviewSchema.validate(req.body);
-    // console.log(result);
-    if(error){
-        let errMsg = error.details.map( (el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
 
 app.use("/listings", listings);
-
-// app.get('/testListing', async (req,res) =>{
-//     let sampleListing = new Listing({
-//         title: "My New Villa",
-//         description: "By the beach",
-//         price: 1500000,
-//         location: "Calangute Goa",
-//         country: "India",
-//     });
-
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("Successfully testing");
-// });
+app.use("/listings/:id/reviews", reviews);
 
 
-//Reviews: Post Route
-app.post("/listings/:id/reviews", validateReview,  wrapAsync(async(req,res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-   res.redirect(`/listings/${listing._id}`);
-}));
 
 
-//Delete review route
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync( async(req,res) => {
-    let {id, reviewId} = req.params;
-    await Listing.findByIdAndUpdate( id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
 
-    res.redirect(`/listings/${id}`);
-}))
   
 app.all("*", (req,res,next) => {
     next(new ExpressError(404, "Page Not Found!"));
