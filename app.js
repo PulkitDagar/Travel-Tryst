96803f15bb959806f50384208958547aa92e41ed
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-04-16 14:29:11 +0530

    changes

--- app.js
+++ app.js
@@ -68,15 +68,15 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get('/demouser', async (req,res) => {
-    let fakeUser = new User({
-        email:"student@gmail.com",
-        username: "student"
-    });
+// app.get('/demouser', async (req,res) => {
+//     let fakeUser = new User({
+//         email:"student@gmail.com",
+//         username: "student"
+//     });
 
-    let registeredUser = await User.register(fakeUser, "helloworld");
-    res.send(registeredUser);
-})
+//     let registeredUser = await User.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+// })
 
 
 app.use((req,res,next) => {
