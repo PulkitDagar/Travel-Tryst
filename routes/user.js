b0c5ba8d49d71fd0c022157f9b3289e63380079c
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-04-18 17:10:11 +0530

    new files

--- routes/user.js
+++ routes/user.js
@@ -14,8 +14,13 @@ router.post("/signup", wrapAsync( async (req,res) => {
         const newUser = new User({email,username});
         const registeredUser = await User.register(newUser, password);
         console.log(registeredUser);
-        req.flash("success", "Welcome to Travel Tryst");
-        res.redirect("/listings");
+        req.login(registeredUser,(err) => {
+            if(err){
+                return next(err);
+            }
+            req.flash("success", "Welcome to Travel Tryst");
+            res.redirect("/listings");
+        })
     } catch(e){
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -30,6 +35,16 @@ router.get("/login", (req,res) => {
 router.post("login", passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}), async(req,res) => {
     req.flash("success","Welcome back to Travel Tryst!");
     res.redirect("/listings");
+})
+
+router.get("/logout", (req,res,next)=> {
+    req.logout( (err) => {
+        if(err) {
+           return next(err);
+        }
+        req.flash("success", "you are logged out!");
+        res.redirect("/listings");
+    })
 })
 
 module.exports = router;
